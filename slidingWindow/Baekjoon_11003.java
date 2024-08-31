5e89487e19d256343dac1b6f5e1b281caf4f0b0e
gwon minji <56202921+mjgwon24@users.noreply.github.com>
2024-08-31 15:08:32 +0900

    Update Baekjoon_11003.java

--- slidingWindow/Baekjoon_11003.java
+++ slidingWindow/Baekjoon_11003.java
@@ -5,6 +5,20 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+/**
+1. 입력 수의 개수 N, 구간을 정하는 수 L, N개의 수 A[N] 입력받기
+2. Deque<Node> mydeque(데이터를 담을 덱 자료구조) 선언
+3. for (N만큼 반복)
+3.1 now (현재 데이터 값) 입력받기
+3.2 덱의 마지막 위치에서부터 now보다 큰 값은 덱에서 제거
+3.3 덱의 마지막 위치에 now값 저장
+3.4 덱의 첫번째 위치에서부터 L의 범위를 벗어난 값(index <= now index - L)을 덱에서 제거
+4.  덱의 첫번째 데이터 출력
+
+1. 덱에 저장할 노드 클래스 별도 생성
+2. 노드 클래스에는 index(자신의 위치), value(자신의 값) 담기
+**/
+
 public class Baekjoon_11003 {
 
     public static void minimumNumberPrint() throws IOException {
