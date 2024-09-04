cdcbd38c37702b08f4b7ef60e4bbf5a7956fa2f0
gwon minji <56202921+mjgwon24@users.noreply.github.com>
2024-09-04 13:48:03 +0900

    [투포인터] baekjoon 1940

--- twoPointer/Baekjoon_1940.java
+++ twoPointer/Baekjoon_1940.java
@@ -1,10 +1,9 @@
-package twoPointer;
-
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * 슈도코드
+ * 1940 주몽
  * 1. 재료의 개수 N, 두 재료 고유번호의 합 M 입력받기
  * 2. 재료 N개 각각의 고유번호 배열 A 입력받기
  * 3. 배열 A 오름차순 정렬
@@ -16,8 +15,8 @@ import java.util.Scanner;
  * 5.3 sum == M 일 경우, i++; j--; cnt++;
  */
 
-public class Baekjoon_1940 {
-    public void calculateArmorCount() {
+class Main {
+    public static void main(String[] args) throws IOException {
         // 1. 재료의 개수 N, 두 재료 고유번호의 합 M 입력받기
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -30,13 +29,13 @@ public class Baekjoon_1940 {
         }
         Arrays.sort(A);
 
-        // 3. 변수 초기화 - 두 고유번호의 합 변수 sum = 0, 시작 인덱스 i = 0, 마지막 인덱스 j = N-1, 만들 수 있는 갑옷 개수 cnt = 0
+        // 4. 변수 초기화 - 두 고유번호의 합 변수 sum = 0, 시작 인덱스 i = 0, 마지막 인덱스 j = N-1, 만들 수 있는 갑옷 개수 cnt = 0
         int sum = 0;
         int i = 0;
         int j = N - 1;
         int cnt = 0;
-
-        // 4. while i < j, 배열 A를 순회하며 갑옷 개수 계산
+        
+        // 5. while i < j, 배열 A를 순회하며 갑옷 개수 계산
         while (i < j) {
             sum = A[i] + A[j];
             if (sum > M) {
