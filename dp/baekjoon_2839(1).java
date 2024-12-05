cc74ec651f5cf9cac0427b62ff71cbd090ce1fc6
gwon minji <alswlchlrh8@naver.com>
2024-12-05 18:06:37 +0900

    [동적 탐색] beakjoon 2839 설탕 배달(1) - DP

--- dp/baekjoon_2839(1).java
+++ dp/baekjoon_2839(1).java
@@ -1,30 +1,30 @@
 import java.util.*;
 
-public class Main {
-	static int[] dp;
+class Main {
     static int N;
+    static int[] dp;
     
     public static void main(String[] args) {
-  		Scanner sc = new Scanner(System.in);
-	    N = sc.nextInt();
-    
-    	// DP 테이블 초기화
-	    dp = new int[N+1];
-    	Arrays.fill(dp, Integer.MAX_VALUE); // 초기값 설정: 만들 수 없는 상태
-	   	dp[0] = 0;
-        
-        //DP 점화식 적용
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+	// dp 테이블 초기화
+        dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE); // 만들 수 없는 상태
+        dp[0] = 0;
+
         for (int i = 3; i <= N; i++) {
-        	if (i >= 3 && dp[i - 3] != Integer.MAX_VALUE) {
-            	dp[i] = Math.min(dp[i], dp[i - 3] + 1);
+            if (i >= 3 && dp[i - 3] != Integer.MAX_VALUE) {
+                dp[i] = dp[i - 3] + 1;
             }
-            
+
             if (i >= 5 && dp[i - 5] != Integer.MAX_VALUE) {
-            	dp[i] = Math.min(dp[i], dp[i - 5] + 1);
+                dp[i] = Math.min(dp[i], dp[i - 5] + 1);
             }
         }
-        
+
         int result = dp[N] == Integer.MAX_VALUE ? -1 : dp[N];
+
         System.out.println(result);
     }
 }
