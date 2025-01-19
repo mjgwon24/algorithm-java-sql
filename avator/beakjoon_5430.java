0dd2ecfbb8919d81962ecfde1650497f1260732d
gwon minji <alswlchlrh8@naver.com>
2025-01-19 12:35:44 +0900

    [구현] baekjoon 5430 AC  - Deque, 구현, 문자열

--- avator/beakjoon_5430.java
+++ avator/beakjoon_5430.java
@@ -1,71 +1,95 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.lang.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws Exception {
         new Main().solution();
     }
     
-    public void solution() throws Exception {
+    public static void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); // 결과값을 저장할 빌더
         
-        // str: 명령어, n: 배열 길이, arrStr: 배열
         for (int i = 0; i < T; i++) {
-            String str = br.readLine();
-            int n = Integer.parseInt(br.readLine());
+            String commandStr = br.readLine(); // 수행할 함수 문자열
+            int n = Integer.parseInt(br.readLine()); // 배열 개수
+            String arrStr = br.readLine(); // 배열 문자열
             
-            String arrStr = br.readLine().replace("[", "").replace("]", "").trim();
+            // 1. 배열문자열을 배열로 변환
+            arrStr = arrStr.replace("[", "");
+            arrStr = arrStr.replace("]", "");
             StringTokenizer st = new StringTokenizer(arrStr, ",");
-            Deque<Integer> dq = new ArrayDeque<>();
             
+            Deque<Integer> dq = new ArrayDeque<>();
             for (int j = 0; j < n; j++) {
-                dq.add(Integer.parseInt(st.nextToken()));
+                dq.offer(Integer.parseInt(st.nextToken().trim()));
             }
             
-            boolean isRight = false;
+            // 2. 각 연산에 따른 동작 수행
+            boolean isReverse = false;
             boolean err = false;
             
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) == 'R') {
-                    isRight = !isRight;
+            for (int j = 0; j < commandStr.length(); j++) {
+                if (commandStr.charAt(j) == 'R') {
+                    isReverse = !isReverse;
                 } else {
                     if (dq.isEmpty()) {
                         err = true;
                         break;
-                    } 
+                    }
                     
-                    if (isRight) dq.pollLast();
-                    else dq.pollFirst();
+                    if (isReverse) {
+                        // 뒤에서 제거
+                        dq.removeLast();
+                    } else {
+                        // 앞에서 제거
+                        dq.removeFirst();
+                    }
                 }
             }
             
-            if (err) sb.append("error");
-            else {
+            if (err) {
+                sb.append("error\n");
+            } else {
                 sb.append("[");
                 
-                while (!dq.isEmpty()) {
-                    if (isRight) sb.append(dq.pollLast());
-                    else sb.append(dq.pollFirst());
-                    
-                    if (dq.size() == 0) break;
-                    else sb.append(",");
+                if (isReverse) {
+                    while (!dq.isEmpty()) {
+                        sb.append(dq.pollLast());
+                        
+                        if (dq.isEmpty()) break;
+                        sb.append(",");
+                    }
+                } else {
+                    while (!dq.isEmpty()) {
+                        sb.append(dq.pollFirst());
+                        
+                        if (dq.isEmpty()) break;
+                        sb.append(",");
+                    }
                 }
                 
-                sb.append("]");
+                sb.append("]\n");
             }
             
-            
-            sb.append("\n");
         }
         
-        System.out.println(sb);
+        // 결과 문자열 출력
+        System.out.print(sb);
     }
 }
 
-// if isRight is true, D -> right first
-// if isRight is false, D -> left first
-// empty array meet command D -> print "error"
-// empty array meet command R -> print "[]"
+// 1. 배열문자열을 배열로 변환
+// 2. 각 연산에 따른 동작 수행
+// 배열 길이가 0일 경우에 D를 수행하면 error 출력
+
+// Deque 앞뒤로 삽입 및 제거 가능 자료형
+
+// R(뒤집기)
+// isReverse = false / true;
+
+// D(맨 앞 숫자 제거)
+// isReverse == false, dq.removeFirst
+// isReverse == true, dq.removeLast
