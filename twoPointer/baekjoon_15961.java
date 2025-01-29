3688f4285aef19bed440da387df3bb6a65949a46
gwon minji <alswlchlrh8@naver.com>
2025-01-29 16:26:04 +0900

    [투포인터] baekjoon 15961 회전초밥  - 슬라이딩윈도우, 메모제이션

--- twoPointer/baekjoon_15961.java
+++ twoPointer/baekjoon_15961.java
@@ -1,6 +1,6 @@
 import java.io.*;
-import java.util.*;
 import java.lang.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws Exception {
@@ -9,14 +9,14 @@ public class Main {
     
     static int N, d, k, c;
     static int[] belt;
-    static int rst;
+    static int result;
     
     public static void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); // 벨트에 놓인 접시 수 (2 ≤ N ≤ 3,000,000)
         d = Integer.parseInt(st.nextToken()); // 초밥 가짓수 (2 ≤ d ≤ 3,000)
-        k = Integer.parseInt(st.nextToken()); // 선택해야하는 접시 수 (2 ≤ k ≤ 3,000 (k ≤ N))
+        k = Integer.parseInt(st.nextToken()); // 선택해아하는 접시 수 (2 ≤ k ≤ 3,000 (k ≤ N))
         c = Integer.parseInt(st.nextToken()); // 추가 초밥 번호 (1 ≤ c ≤ d)
         belt = new int[N];
         
@@ -24,59 +24,49 @@ public class Main {
             belt[i] = Integer.parseInt(br.readLine());
         }
         
+        // 1. 길이가 k인 슬라이딩 윈도우 실행
         sliding(N == k ? true : false);
-        System.out.println(rst);
+        System.out.println(result);
     }
     
+    // 1. 길이가 k인 슬라이딩 윈도우 실행
+    // 도는 횟수는 총 N번, 만약 N == k 라면 도는 횟수는 1번
     static void sliding(boolean isOne) {
         int left = 0;
         int right = k - 1; // 3
-        int cnt = 0;
         int[] equalCnt = new int[d + 1];
-        int nowSelect = 1;
-        boolean isSelectPlus = false;
-        equalCnt[c] = Integer.MAX_VALUE;
+        equalCnt[c] = Integer.MAX_VALUE; // 추가 초밥은 언제나 선택되어있는 상태
+        // int cnt = 0; // 연산 횟수
+        int select = 1; // 현재 범위에서 선택한 초밥의 가짓수 (추가 초밥은 언제나 선택되어있는 상태)
         
-        if (isOne) cnt = N - 1;
-        
-        // 1. k-1개를 미리 초기화
-        for (int i = 0; i < right; i++) { // 0, 1, 2 초기화 필요
+        // 초기화 - 중복 초밥 카운트
+        for (int i = 0; i < right; i++) { // 0, 1, 2
             equalCnt[belt[i]]++;
-            if (equalCnt[belt[i]] == 1) nowSelect++;
+            if (equalCnt[belt[i]] == 1) select++;
         }
-        rst = nowSelect;
         
-        // 2. right를 1씩 늘려가며 탐색
-        while (cnt < N) {
+        if (isOne) left = N - 1;
+        
+        while (left < N) {
+            // right 초밥 계산
             equalCnt[belt[right % N]]++;
-            if (equalCnt[belt[right % N]] == 1) nowSelect++;
+            if (equalCnt[belt[right % N]] == 1) select++;
+            // System.out.printf("%d session, plus equalCnt[%d] = %d, select = %d\n", left, belt[right % N], equalCnt[belt[right % N]], select);
             
-            rst = Math.max(rst, nowSelect);
+            // 최대 초밥 수 갱신
+            result = Math.max(select, result);
             
-            // 돌리기
-            // System.out.printf("돌리기 전 equalCnt[%d] = %d\n", belt[left % N], equalCnt[belt[left % N]]);
-            equalCnt[belt[left % N]]--;
-            // System.out.printf("돌린 후 equalCnt[%d] = %d\n", belt[left % N], equalCnt[belt[left % N]]);
-            if (equalCnt[belt[left % N]] == 0) {
-                nowSelect--;
-                // System.out.printf("제거된 초밥 종류: %d", belt[left % N]);
-            }
-            // System.out.println();
-            // System.out.println();
+            // 이동
+            equalCnt[belt[left]]--;
+            if (equalCnt[belt[left]] == 0) select--;
+            // System.out.printf("%d session, minus equalCnt[%d] = %d, select = %d\n\n", left, belt[left], equalCnt[belt[left]], select);
             left++;
             right++;
-            cnt++;
         }
     }
 }
 
-// 회전 초밥 벨트에서 먹을 수 있는 초밥의 가짓수의 최댓값 출력
-// 추가 초밥이 추가되는 경우 고려
-// 벨트를 슬라이딩 윈도우로 탐색
+// 먹을 수 있는 초밥 가짓수 최댓값 도출
 
-// 1. k-1개를 미리 초기화
-// 2. right를 1씩 늘려가며 탐색
-//    접시 종류 중복 카운트
-// 3. 추가 초밥이 선택되지 않은 경우에는 +1
-// 4. 최종 선택가능 가짓수 출력
-// 탐색을 시도하는 총 횟수: N번
+// 1. 길이가 k인 슬라이딩 윈도우 실행
+// 2. 가짓수 최댓값 갱신 및 최종 결과 출력
