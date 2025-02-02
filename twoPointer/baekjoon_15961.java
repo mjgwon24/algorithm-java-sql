799856aa32854938f3e4abc87a450a09a02324c4
gwon minji <alswlchlrh8@naver.com>
2025-02-02 19:53:49 +0900

    [투포인터] baekjoon 15961 회전초밥  - 슬라이딩윈도우, 메모제이션 (시간복잡도 및 값 범위 추가)

--- twoPointer/baekjoon_15961.java
+++ twoPointer/baekjoon_15961.java
@@ -1,6 +1,6 @@
 import java.io.*;
-import java.lang.*;
 import java.util.*;
+import java.lang.*;
 
 public class Main {
     public static void main(String args[]) throws Exception {
@@ -14,59 +14,63 @@ public class Main {
     public static void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 벨트에 놓인 접시 수 (2 ≤ N ≤ 3,000,000)
+        N = Integer.parseInt(st.nextToken()); // 벨트 접시 수 (2 ≤ N ≤ 3,000,000)
         d = Integer.parseInt(st.nextToken()); // 초밥 가짓수 (2 ≤ d ≤ 3,000)
-        k = Integer.parseInt(st.nextToken()); // 선택해아하는 접시 수 (2 ≤ k ≤ 3,000 (k ≤ N))
+        k = Integer.parseInt(st.nextToken()); // 선택하는 초밥 범위 (2 ≤ k ≤ 3,000)
         c = Integer.parseInt(st.nextToken()); // 추가 초밥 번호 (1 ≤ c ≤ d)
         belt = new int[N];
         
         for (int i = 0; i < N; i++) {
-            belt[i] = Integer.parseInt(br.readLine());
+            belt[i] = Integer.parseInt(br.readLine()); // 벨트 위의 초밥
         }
         
-        // 1. 길이가 k인 슬라이딩 윈도우 실행
-        sliding(N == k ? true : false);
+        // 슬라이딩 윈도우로 초밥 가짓수 계산
+        sliding();
         System.out.println(result);
     }
     
-    // 1. 길이가 k인 슬라이딩 윈도우 실행
-    // 도는 횟수는 총 N번, 만약 N == k 라면 도는 횟수는 1번
-    static void sliding(boolean isOne) {
+    // 슬라이딩 윈도우로 초밥 가짓수 계산
+    static void sliding() {
         int left = 0;
         int right = k - 1; // 3
-        int[] equalCnt = new int[d + 1];
-        equalCnt[c] = Integer.MAX_VALUE; // 추가 초밥은 언제나 선택되어있는 상태
-        // int cnt = 0; // 연산 횟수
-        int select = 1; // 현재 범위에서 선택한 초밥의 가짓수 (추가 초밥은 언제나 선택되어있는 상태)
+        int selectCnt = 1; // 추가 초밥은 항상 선택하고있다고 가정
+        int[] equalSushi = new int[3001];
+        equalSushi[c] = N + 1; // 추가 초밥 예외처리
         
-        // 초기화 - 중복 초밥 카운트
+        // 1. 초기화
         for (int i = 0; i < right; i++) { // 0, 1, 2
-            equalCnt[belt[i]]++;
-            if (equalCnt[belt[i]] == 1) select++;
+            equalSushi[belt[i]]++;
+            if (equalSushi[belt[i]] == 1) selectCnt++; // 처음으로 추가된 초밥의 경우 선택 개수 증가
         }
         
-        if (isOne) left = N - 1;
-        
+        // 2. right를 하나씩 늘려가면서 추가되는 초밥 탐색, 중복 초밥 갱신
+        // 최대 N회 반복 가능 (단, k가 N일시 1회 반복)
         while (left < N) {
-            // right 초밥 계산
-            equalCnt[belt[right % N]]++;
-            if (equalCnt[belt[right % N]] == 1) select++;
-            // System.out.printf("%d session, plus equalCnt[%d] = %d, select = %d\n", left, belt[right % N], equalCnt[belt[right % N]], select);
+            equalSushi[belt[right % N]]++;
+            if (equalSushi[belt[right % N]] == 1) selectCnt++;
             
-            // 최대 초밥 수 갱신
-            result = Math.max(select, result);
+            // 4. 최종 초밥 가짓수 갱신
+            result = Math.max(result, selectCnt);
             
-            // 이동
-            equalCnt[belt[left]]--;
-            if (equalCnt[belt[left]] == 0) select--;
-            // System.out.printf("%d session, minus equalCnt[%d] = %d, select = %d\n\n", left, belt[left], equalCnt[belt[left]], select);
-            left++;
+            // left 이동시 중복 초밥 배열 갱신
+            if (equalSushi[belt[left]] == 1) selectCnt--;
+            equalSushi[belt[left]]--;
             right++;
+            left++;
         }
     }
 }
 
-// 먹을 수 있는 초밥 가짓수 최댓값 도출
+// 주어진 회전 초밥 벨트에서 먹을 수 있는 초밥 가짓수 최댓값 도출
 
-// 1. 길이가 k인 슬라이딩 윈도우 실행
-// 2. 가짓수 최댓값 갱신 및 최종 결과 출력
+// 슬라이딩 윈도우로 초밥 가짓수 계산
+// 1. 초기화
+// 2. right를 하나씩 늘려가면서 추가되는 초밥 탐색, 중복 초밥 갱신
+// 3. 추가 초밥 고려
+// 4. 최종 초밥 가짓수 갱신
+
+// 나올 수 있는 최댓값: N + 1 => 3,000,001
+// int 범위: 2 * 10^9 (안전)
+
+// 시간복잡도: N => 3 * 10^6
+// 1초: 연산 10^8 (안전)
