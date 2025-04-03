593f4cfaef13c7941dd36895de0de77c45a76395
gwon minji <alswlchlrh8@naver.com>
2025-04-03 11:35:52 +0900

    [DFS] 8913 문자열 뽑기 - BufferedWriter

--- dfs/baekjoon_8913.java
+++ dfs/baekjoon_8913.java
@@ -1,43 +1,43 @@
 import java.io.*;
+import java.util.*;
 import java.lang.*;
 
 public class Main {
-    static int N;
     static boolean success;
-
+    
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int t = 0; t < T; t++) {
             String str = br.readLine();
+            success = false;
             dfs(str);
 
             if (success) bw.write(1 + "\n");
             else bw.write(0 + "\n");
-
-            success = false;
+        
         }
 
+        //  빈 문자로 바꿀 수 있는지 없는지 "여부" 확인
         bw.flush();
     }
 
     static void dfs(String s) {
         if (success) return;
 
-        int stst = 1; // 연속된 문자 개수
-        int startIdx = 0; // 그룹 시작 인덱스
+        int stst = 1; // 연속된 문자열 개수
+        int startIdx = 0;
 
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i - 1) == s.charAt(i)) {
-                // 이전 문자와 현재 문자가 동일하다면 연속된 문자 증가
+                // 이전 문자와 현재 문자가 같다면, 그룹 문자열 길이 늘리기
                 stst++;
             } else {
-                // 이전 문자와 현재 문자가 다른 경우
+                // 이전 문자와 현재 문자가 다르다면, 그룹 문자열을 제거하고 인덱스 초기화
                 if (stst >= 2) {
-                    // 연속된 문자가 2개 이상이라면 제거 가능
-                    dfs(s.substring(0, startIdx) + s.substring(startIdx + stst, s.length()));
+                    dfs(s.substring(0, startIdx) + s.substring(startIdx + stst));
                 }
 
                 stst = 1;
@@ -45,7 +45,12 @@ public class Main {
             }
         }
 
-        // 문자열 전체가 같은 문자로 이루어져있고 길이가 2 이상이라면 true
+        // 들어온 s가 2 이상이고 전부 그룹 문자열이라면 true
         if (s.length() >= 2 && s.length() == stst) success = true;
     }
 }
+
+/***
+• DFS - "여부" 한번만 확인
+• BFS - 모든 경우를 탐색해 최단 거리/횟수 도출
+***/
