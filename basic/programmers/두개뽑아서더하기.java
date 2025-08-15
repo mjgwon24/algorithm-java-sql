248f0588238f60359c48d5f0d75e1775a31e9c83
gwon minji <alswlchlrh8@naver.com>
2025-08-16 03:45:50 +0900

    [TreeSet] 두개뽑아서더하기

--- basic/programmers/두개뽑아서더하기.java
+++ basic/programmers/두개뽑아서더하기.java
@@ -2,23 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        // numbers에서 서로 다른 인덱스에 있는 두 수를 뽑아 더해서 만들 수 있는 모든 수 도출
-        Set<Integer> set = new HashSet<>();
+        TreeSet<Integer> ts = new TreeSet<>();
         
-        for (int i = 0; i < numbers.length - 1; i++) {
+        // 서로 다른 인덱스 두 수 합산
+        for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
-                set.add(numbers[i] + numbers[j]);
+                ts.add(numbers[i] + numbers[j]);
             }
         }
         
-        List<Integer> list = new ArrayList<>(set);
-        list.sort((a, b) -> a - b);
-        
-        int[] answer = new int[list.size()];
+        int[] answer = new int[ts.size()];
         int idx = 0;
-        for (int n: list) {
-            answer[idx++] = n;
+        while (!ts.isEmpty()) {
+            answer[idx++] = ts.pollFirst();
         }
         return answer;
+        
     }
 }
